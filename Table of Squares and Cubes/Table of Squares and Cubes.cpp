5ed550a1e2c59318a30a567781f268ed505bc3e2
Chip <59837459+ComnHomeAce@users.noreply.github.com>
2021-04-18 21:42:40 -0700

    Update Table of Squares and Cubes.cpp

--- Table of Squares and Cubes/Table of Squares and Cubes.cpp
+++ Table of Squares and Cubes/Table of Squares and Cubes.cpp
@@ -11,12 +11,17 @@ int main()
 
     cout << "This program will calculate the squares and cubes for the numbers 1-10\n\n";
 
+    //Tabel header
+    cout << "number  square  cube" << endl;
     while (i < 11)
     {
         //This loop will calculate the square and cobe of the index, before incrementing the index by 1
-
         cout << left;
         cout << setw(8) << i;
+        cout << setw(8) << pow(i, 2);
+        cout << setw(8) << pow(i, 3) << endl;
+
+        //Increments index
         i++;
     }
 }
